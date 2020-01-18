8922bf912e54ef36b5c3f19e3217a930df150cd3
Jaspreet Singh <jaspreetjhass91@gmail.com>
2020-01-18 23:18:59 +0530

    added JDBC authenication to the repo

---
+++ JDBCAuthenticationDemo/src/main/java/com/example/demo/JdbcAuthenticationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcAuthenticationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcAuthenticationDemoApplication.class, args);
+	}
+
+}
